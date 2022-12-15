25669b0fd584c18e4a8ecc89371c1034577651cc
Felix Xing <101950667+felixxing@users.noreply.github.com>
2022-12-15 14:13:40 -0500

    glm

---
+++ include/glms.hpp
@@ -0,0 +1,11 @@
+#ifndef GLMS_HPP
+#define GLMS_HPP
+
+#define GLM_FORCE_INLINE
+#define GLM_FORCE_XYZW_ONLY
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#endif // GLMS_HPP
